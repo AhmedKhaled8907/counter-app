7e6daea015e9775d25f63d8778db37cf250443d9
Ahmed khaled <ahmedkh8907@gmail.com>
2023-12-26 16:45:23 +0200

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,72 @@
+import 'package:counter_app/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /*
+  The issue in your code is that you are creating
+  a new instance of CounterCubit each time you call
+  CounterCubit() in the BlocBuilder and the onPressed callback.
+  This means that the BlocBuilder is listening to
+  a different instance of CounterCubit than the one
+  you are incrementing in the onPressed callback.
+
+  To fix this, you should use the same instance of CounterCubit
+  in both places. Create an instance of CounterCubit
+  in the State of your widget and use that instance consistently.
+  */
+  final counterCubit = CounterCubit();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterCubit, int>(
+              bloc: counterCubit,
+              builder: (context, counter) {
+                return Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => counterCubit.increment(),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () => counterCubit.decrement(),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
